9ab1bb00e1329dd8e38ae60636e21e02970bf946
Prabhaker Manikyam <prabhakarmanikyam24@gmail.com>
2025-06-27 19:43:26 -0700

    Update cumulative _table_design_pipeline_query.sql

--- cumulative _table_design_pipeline_query.sql
+++ cumulative _table_design_pipeline_query.sql
@@ -1,6 +1,8 @@
+
+--To retrieve data from actor_films
 select * from actor_films;
 
-drop type films;
+-- creating films struct 
 create type films as(
 film TEXT,
 votes INTEGER,
@@ -8,53 +10,210 @@ rating REAL,
 filmid TEXT
 )
 
-
+-- to create enumerated quality_class for star, good, average, bad
 create type quality_class as enum('star','good','average','bad');
 
- CREATE TABLE actors (
+
+-- creating actors table
+CREATE TABLE actors (
     actor TEXT,
     actorid TEXT,
-  films films[],
-     quality_class quality_class,
+    films films[],
+    quality_class quality_class,
     is_active BOOLEAN,
     this_year INT,
-   PRIMARY KEY (actorid, this_year)
- );
+    PRIMARY KEY (actorid, this_year)
+);
 
 
-select min(year) from actor_films;
+-- cumulative table design query where we add films of an actor per year 
 
-WITH this_year as (
-select actor,
-	   actorid,
-	   year,
-	   ARRAY_AGG(ROW(film, votes, rating, filmid)::films) AS film_array,
-	   AVG(rating) as avg_rating
-	   from actor_films
-	   where year = 1971
-	   group by actor, actorid, year
-), last_year as(
-select * from actors where this_year = 1970
+INSERT INTO actors
+WITH years AS (
+    SELECT * FROM GENERATE_SERIES(1970, 2022) AS year
+), p AS (	
+SELECT
+        actor,
+        actorid,
+        MIN(year) AS first_year,
+		row_number() over(partition by actor order by actorid) as row_number
+    FROM actor_films
+    GROUP BY actor, actorid
+), actor_and_years AS (
+    SELECT *
+    FROM p
+    JOIN years y ON p.first_year <= y.year where row_number = 1
+), aggregated AS (
+    SELECT
+        a.actor,
+        a.actorid,
+        y.year,
+        ARRAY_AGG(
+            ROW(f.film, f.votes, f.rating, f.filmid)::films
+        ) FILTER (WHERE f.film IS NOT NULL) AS films
+    FROM actor_and_years a
+    JOIN years y ON a.year = y.year
+    LEFT JOIN actor_films f
+        ON a.actor = f.actor AND a.year = f.year
+    GROUP BY a.actor, a.actorid, y.year
 )
-INSERT into actors
-     select 
-	 coalesce(ly.actor,ty.actor) as actor,
-	 coalesce(ly.actorid,ty.actorid) as actorid,
-	 coalesce(ly.films,ARRAY[]::films[]) || coalesce(ty.film_array,ARRAY[]::films[]) as films,
-	 CASE 
-	 WHEN ty.year is not null then 
-	 (case when ty.avg_rating > 8.0 then 'star'
-	       when ty.avg_rating > 7.0 then 'good'
-		   when ty.avg_rating > 6.0 then 'average'
-		   else 'bad' end)::quality_class
-		   else ly.quality_class
-		   end as quality_class,
-	ty.year is not null as is_active,
-	coalesce(ty.year,ly.this_year+1) as year
+SELECT
+    actor,
+    actorid,
+    COALESCE(films, ARRAY[]::films[]) AS films,
+    CASE
+        WHEN CARDINALITY(COALESCE(films, ARRAY[]::films[])) = 0 THEN 'bad'
+        WHEN (films[CARDINALITY(films)]::films).rating > 8.0 THEN 'star'
+        WHEN (films[CARDINALITY(films)]::films).rating > 7.0 THEN 'good'
+        WHEN (films[CARDINALITY(films)]::films).rating > 6.0 THEN 'average'
+        ELSE 'bad'
+    END::quality_class AS quality_class,
+    CARDINALITY(COALESCE(films, ARRAY[]::films[])) > 0 AS is_active,
+    year AS this_year
+FROM aggregated;
 
-	FROM last_year ly
-	FULL OUTER JOIN this_year ty
-	ON   ly.actorid = ty.actorid 
-	 
-  
-select * from actors;
+
+-- to create table for slowly changin dimensions with actors_history_scd
+
+create table actors_history_scd(
+	actor text,
+	quality_class quality_class,
+	is_active boolean,
+	start_date integer,
+	end_date integer,
+	this_year integer,
+	primary key(actor, start_date)
+)
+
+
+-- inserting the actors history with slowly changing dimensions with backfill query from 2021
+insert into actors_history_scd
+with with_previous as(
+select 	
+		actor,
+		quality_class,
+		this_year,
+		is_active,
+		LAG(quality_class,1) over (partition by actor order by this_year) as previous_quality_class,
+		LAG(is_active,1) over (partition by actor order by this_year) as previous_is_active
+from actors where this_year <= 2021 
+),
+with_indicators as(
+select *, 
+	case when quality_class <> previous_quality_class then 1 
+    when is_active <> previous_is_active then 1 
+	else 0
+	end as change_indicator
+from with_previous
+),
+with_streaks as(
+select * ,
+		sum(change_indicator) 
+				over(partition by actor order by this_year) as streak_identifier
+from with_indicators
+)
+	select actor,
+	quality_class,
+	is_active,
+	min(this_year) as start_date,
+	max(this_year) as end_date,
+	2021 as this_year
+	from with_streaks
+	group by actor, streak_identifier, is_active, quality_class
+	order by actor, streak_identifier
+
+--to retrieve data from actors_history_scd table
+select * from actors_history_scd;
+
+
+-- creating scd-type for changed records where there is change occured
+create type  actors_scd_type as(
+quality_class quality_class,
+is_active boolean,
+start_date integer,
+end_date integer
+)
+
+-- Incremental fill query for actors_history_scd with slowly changing dimensions
+
+with last_year_scd as(
+select * from actors_history_scd
+where this_year = 2021
+),
+historical_scd as(
+select 	actor,
+		quality_class,
+		is_active,
+		start_date,
+		end_date
+from actors_history_scd
+where this_year = 2021 and this_year < 2022
+),
+ this_year_data as(
+select * from actors where this_year = 2022
+ ),
+ unchanged_records as(
+	select ty.actor,
+		ty.quality_class,
+		ty.is_active,
+		ly.start_date,
+		ty.this_year as end_date
+	from this_year_data ty 
+	 JOIN last_year_scd ly
+	ON ly.actor = ty.actor 
+	where ty.quality_class = ly.quality_class
+	and ty.is_active = ly.is_active
+ ),
+ changed_records as(
+	select ty.actor,
+		ty.quality_class,
+		ty.is_active,
+		ly.start_date,
+		ty.this_year as end_date,
+		UNNEST(ARRAY[
+			ROW(
+				ly.quality_class,
+				ly.is_active,
+				ly.start_date,
+				ly.end_date
+			)::actors_scd_type,
+			ROW(
+				ty.quality_class,
+				ty.is_active,
+				ty.this_year,
+				ty.this_year
+			)::actors_scd_type
+		]) as records
+	from this_year_data ty 
+	 LEFT JOIN last_year_scd ly
+	ON ly.actor = ty.actor 
+	where (ty.quality_class <> ly.quality_class
+	or ty.is_active <> ly.is_active)
+ ),
+ unnested_changed_records as(
+select actor, 
+	(records::actors_scd_type).quality_class,
+	(records::actors_scd_type).is_active,
+	(records::actors_scd_type).start_date,
+	(records::actors_scd_type).end_date
+	from changed_records
+ ),
+ new_records as(
+	select ty.actor,
+			ty.quality_class,
+			ty.is_active,
+			ty.this_year as start_date,
+			ty.this_year as end_date
+	from this_year_data ty
+	LEFT JOIN last_year_scd ly
+		ON ty.actor = ly.actor
+		where ly.actor is null
+ )
+
+select * from historical_scd
+ UNION all
+select * from unchanged_records
+union all
+select * from unnested_changed_records
+union all
+select * from new_records
